54e642a60b74136ba25d38e89673931201ce3b4b
Mari <165931583+Mmmmari@users.noreply.github.com>
2024-05-18 12:15:31 +0300

    Create 18.05.2024.py

---
+++ 18.05.2024.py
@@ -0,0 +1,8 @@
+# Team work: Write a code example how to iterate through a dictionary with a for-loop.
+
+
+
+dictionary = {'Minnie': 80, 'Donald': 90, 'Mickey': 100}
+
+for key, value in dictionary.items():
+	print(f"Character {key} is {value} years old.")
