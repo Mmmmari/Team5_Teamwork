f12d3e37835bb260db9839f9747fee411a74d718
Mari <165931583+Mmmmari@users.noreply.github.com>
2024-05-29 20:11:53 +0300

    Create 29.05.2024.Phone.java

---
+++ 29.05.2024.Phone.java
@@ -0,0 +1,14 @@
+public class Phone {
+  public String brand;
+  public String color;
+  public int price;
+  public boolean used;
+
+  public Phone(String brand, String color, int price, boolean used) {
+    this.brand = brand;
+    this.color = color;
+    this.price = price;
+    this.used = used;
+  }
+
+}
